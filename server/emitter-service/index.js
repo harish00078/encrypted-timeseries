0751f88468d0f5e8663fc34954130ef5944858b1
harish kumar <harishpathania150@gmail.com>
2023-09-02 21:15:33 +0530

    i have updated the code

--- server/emitter-service/index.js
+++ server/emitter-service/index.js
@@ -1,11 +1,11 @@
 // emitter.js
 
-const net = require('net');
-const crypto = require('crypto');
-const fs = require('fs');
+const net = require("net");
+const crypto = require("crypto");
+const fs = require("fs");
 
-const data = JSON.parse(fs.readFileSync('data.json'));
-const passKey = 'blahsomething'; // Replace with your pass key
+const data = JSON.parse(fs.readFileSync("data.json"));
+const passKey = "blahsomething"; // Replace with your pass key
 
 const server = net.createServer((socket) => {
   setInterval(() => {
@@ -16,16 +16,20 @@ const server = net.createServer((socket) => {
       const randomIndex = Math.floor(Math.random() * data.length);
       const message = data[randomIndex];
 
-      const cipher = crypto.createCipher('aes-256-ctr', passKey);
-      const encryptedMessage = cipher.update(JSON.stringify(message), 'utf8', 'hex');
+      const cipher = crypto.createCipher("aes-256-ctr", passKey);
+      const encryptedMessage = cipher.update(
+        JSON.stringify(message),
+        "utf8",
+        "hex"
+      );
       encryptedMessages.push(encryptedMessage);
     }
 
-    const messageStream = encryptedMessages.join('|');
+    const messageStream = encryptedMessages.join("|");
     socket.write(messageStream);
   }, 10000); // Emit every 10 seconds
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Emitter server started on port 3000');
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Emitter server started on port 3000");
 });
